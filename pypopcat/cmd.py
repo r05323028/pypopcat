dd555f1ba41843e25578f83a97b296f6df23b137
Sean Chang <seanchang@kklab.com>
2021-08-18 15:57:14 +0800

    Add proxy support

--- pypopcat/cmd.py
+++ pypopcat/cmd.py
@@ -12,14 +12,31 @@ def cli():
     '''pypopcat command line interface
     '''
 
+
 @click.command()
-@click.option('--limit', type=int, help="total number of clicks, None is infinite loop.")
+@click.option('--limit',
+              type=int,
+              help="total number of clicks, None is infinite loop.")
 @click.option('--country', default='TW', help='country code')
 @click.option('--n_threads', default=1, type=int, help="number of windows")
-def emit(limit, country, n_threads):
+@click.option('--proxy_server', default=None, type=str)
+def emit(limit, country, n_threads, proxy_server):
     '''emit clicks on popcat.click
     '''
-    with concurrent.futures.ThreadPoolExecutor(max_workers=n_threads) as executor:
-        futures = {executor.submit(emit_clicks, HOST, RATE_LIMIT, limit, country, worker_id=i) for i in range(n_threads)}
+    with concurrent.futures.ThreadPoolExecutor(
+            max_workers=n_threads) as executor:
+        futures = {
+            executor.submit(
+                emit_clicks,
+                HOST,
+                RATE_LIMIT,
+                limit,
+                country,
+                worker_id=i,
+                proxy_server=proxy_server,
+            )
+            for i in range(n_threads)
+        }
+
 
 cli.add_command(emit)
--- pypopcat/emit.py
+++ pypopcat/emit.py
@@ -1,9 +1,10 @@
 import logging
-import time
 from typing import Optional
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from fake_useragent import UserAgent
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -14,26 +15,32 @@ def emit_clicks(
     rate_limit: float,
     limit: Optional[int] = None,
     country: str = 'TW',
-    worker_id: int = 1,
+    worker_id: int = 0,
+    proxy_server: Optional[str] = None,
 ):
     '''emit clicks
 
     Args:
         host (str): hostname
         rate_limit (float): rate limit
-        limit (int): total number of clicks. optional
+        limit (Optional[int]): total number of clicks. optional
         country (str): country code
         worker_id (int): worker id
+        proxy_server (Optional[str]):  proxy server
     '''
+    ua = UserAgent()
     options = webdriver.ChromeOptions()
     options.add_argument('--mute-audio')
+    if proxy_server:
+        logger.info(f'Start chrome by proxy: {proxy_server}')
+        options.add_argument(f'--incognito --proxy-server={proxy_server} --user-agent={ua.google}')
     driver = webdriver.Chrome(options=options)
     driver.get(host)
     driver.add_cookie({
         'name': "country",
         "value": country,
     })
-    app = driver.find_element_by_id('app')
+    app = driver.find_element(By.ID, 'app')
     i = 0
 
     while True:
@@ -42,7 +49,7 @@ def emit_clicks(
         actions.perform()
         i += 1
         logger.info("Worker: %s, Click Stats: %s", worker_id, i)
-        time.sleep(rate_limit)
+        driver.implicitly_wait(rate_limit)
 
         if limit and i >= limit:
             break
